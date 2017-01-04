154d4189d4f0254ca7c90d576b8de9b2e3ec3e55
TimeTravel-1 <davidng@Davids-MacBook-Air-2.local>
2017-01-03 21:34:27 -0700

    Removed folder

--- Assignments/Exemplars/assn3-1.0-solution-MARKING-4.0/org.lsmr.vending.frontend3-MARKING/src/org/lsmr/vending/frontend3/hardware/EmptyException.java
+++
@@ -1,8 +0,0 @@
-package org.lsmr.vending.frontend3.hardware;
-
-/**
- * Represents the situation when a device is emptied but an attempt is made to
- * remove something from it.
- */
-@SuppressWarnings("serial")
-public class EmptyException extends Exception {}
